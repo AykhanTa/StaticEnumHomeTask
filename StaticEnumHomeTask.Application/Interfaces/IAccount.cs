54209a8397386db943b85c93e5361a11e24a9801
AykhanTa <aykhanft@code.edu.az>
2024-03-13 00:12:20 +0400

    Add project files.

---
+++ StaticEnumHomeTask.Application/Interfaces/IAccount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticEnumHomeTask.Application.Interfaces
+{
+    internal interface IAccount
+    {
+        void Login(string username, string password);
+    }
+}
